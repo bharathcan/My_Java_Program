3478b4baa1177a3485f579c299b1b280a413e8e2
bharathcan <putta.bharathnaidu@gmail.com>
2019-02-16 20:09:19 +0530

    My First Commit

---
+++ N.java
@@ -0,0 +1,6 @@
+class C
+{
+byte b=123;
+short s=9999;
+s=b;
+public static void main(string[]args)  
---
+++ Operators.java
@@ -0,0 +1,22 @@
+class Operators
+{
+public static void main(String[] args)
+{
+int a =10, b=20, c=12;
+String X="bharath" ,Y="Putta";
+
+System.out.println("a+b" +(a+b)); 
+
+System.out.println("a-b" +(a-b));
+
+System.out.println("a*b" +(a*b));
+
+
+System.out.println("a/b" +(a/b));
+
+
+System.out.println("a%b" +(a%b));
+
+System.out.println("X+Y" +(X+Y));
+}
+}
